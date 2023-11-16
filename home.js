99849e808d4671cffcdcfb8d87411089d6a6a43b
Crow <quiroganicolas0698@gmail.com>
2023-11-16 19:50:00 -0300

    act

---
+++ home.js
@@ -0,0 +1,13 @@
+
+
+function mostrar(){
+
+    document.getElementById("autos").style.display = "grid";
+
+}
+
+function ocultar(){
+
+    document.getElementById("autos").style.display = "none";
+    
+}
